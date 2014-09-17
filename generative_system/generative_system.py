33d06aa76e2241a126af05dfbe10eb87035c6fdd
Alexander T J Barron <atbarron@indiana.edu>
2014-09-16 20:23:40 -0400

    Mask submask generator now abides by all requirements of 'meaningful submasks' (p. 100 ASPS).

--- generative_system/generative_system.py
+++ generative_system/generative_system.py
@@ -19,10 +19,13 @@ class Mask(object):
             for c in it.combinations(xrange(self.maxdepth), r):
                 yield c
         
-    def gen_rowinds(self, gen_stack, ind_stack, rind):
+    def gen_inds(self, gen_stack, ind_stack, rind):
         """
-        Generates tuples of indices for masking a numpy array of data.
+        Generates lists of indices for masking a numpy array of data.
         Intended to be called from the mask_generator method.
+
+        Index lists correspond to meaningful submasks as defined on p. 100 of
+        Archetecture of Systems Problem Solving by George Klir and Doug Elias.
 
         Args:
           gen_stack (list): list of _powerset generators, length equal to
@@ -42,14 +45,17 @@ class Mask(object):
                     iindspre = [[i]*len(inds) for i, inds in enumerate(ind_stack)]
                     iinds = reduce(lambda x, y: list(x) + list(y), iindspre)
                     jinds = reduce(lambda x, y: list(x) + list(y), ind_stack)
-                    yield iinds, jinds
+                    if self.maxdepth - 1 not in jinds:
+                        pass # for condition m2 on p. 100
+                    else:
+                        yield [iinds, jinds]
                 else:
                     rind += 1
-                self.gen_rowinds(gen_stack, ind_stack, rind)
+                self.gen_inds(gen_stack, ind_stack, rind)
             except StopIteration:
                 gen_stack[rind] = self._powerset()
                 rind -= 1
-                self.gen_rowinds(gen_stack, ind_stack, rind)
+                self.gen_inds(gen_stack, ind_stack, rind)
 
     def mask_generator(self):
         """
@@ -63,4 +69,4 @@ class Mask(object):
         gen_stack = [self._powerset() for j in range(self.vnum)]
         ind_stack = [0]*self.vnum
         rind = 0
-        return self.gen_rowinds(gen_stack, ind_stack, rind)
+        return self.gen_inds(gen_stack, ind_stack, rind)
