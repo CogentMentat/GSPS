852c31d87f3750e764728a4722860d991d29f63a
Alexander TJ Barron <atbarron@umail.iu.edu>
2018-03-27 16:23:57 -0400

    Fix mask generator, alter generative system storage of seen variable states, add function for finding admissible behavior systems

--- generative_system/generative_system.py
+++ generative_system/generative_system.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 """
 Functions for creating a generative system on a curated data set.
 
@@ -5,10 +6,11 @@ Author(s): Alexander TJ Barron
 Date created: 2014-09-07
 """
 
-import itertools as it
 from collections import defaultdict
-import pandas as pd
+from itertools import combinations
+from functools import reduce
 import numpy as np
+
 import pdb
 
 class M_matrix(object):
@@ -30,7 +32,7 @@ class M_matrix(object):
     
     def _powerset(self):
         for r in range(1,self.maxdepth + 1):
-            for c in it.combinations(xrange(self.maxdepth), r):
+            for c in combinations(range(self.maxdepth), r):
                 yield c
         
     def gen_inds(self, gen_stack, ind_stack, rind, increasing):
@@ -58,56 +60,76 @@ class M_matrix(object):
 
         """
 
-        def generative_inds(ind_stack, increasing):
-            """Produce generative indices."""
-            
-            generating_iindspre = \
-                    [[i]*(len(inds) - 1) for i, inds in \
-                    enumerate(ind_stack)]
-            generating_iinds = \
-                    reduce(lambda x, y: list(x) + list(y),
-                           generating_iindspre)
-            
-            if increasing:
-                generated_iinds = range(self.vnum)
-                generating_jinds = \
-                        reduce(lambda x, y: list(x)[:-1] + \
-                                            list(y)[:-1],
-                               ind_stack)
-                generated_jinds = [ind[-1] for ind in ind_stack]
-            else:
-                generated_iinds = range(self.vnum)
-                generating_jinds = \
-                        reduce(lambda x, y: list(x)[1:] + \
-                                            list(y)[1:],
-                               ind_stack)
-                generated_jinds = [ind[0] for ind in ind_stack]
-
-            generated_inds = [generated_iinds, generated_jinds]
-            generating_inds = [generating_iinds, generating_jinds]
-
-            return generated_inds, generating_inds
-
-        # actual recursive generator
+        # Recursive generator.
         while rind > -1:
             try:
-                ind_stack[rind] = gen_stack[rind].next()
+                ind_stack[rind] = next(gen_stack[rind])
                 if rind == self.vnum - 1:
-                    jinds = reduce(lambda x, y: list(x) + list(y),
-                                   ind_stack)
-                    if self.maxdepth - 1 not in jinds:
-                        pass # for condition m2 on p. 100
+
+                    iindspre = [[i]*len(inds) for i, inds in \
+                                enumerate(ind_stack)]
+                    jindspre = [list(inds) for inds in ind_stack]
+
+                    if not any([self.maxdepth - 1 in inds for inds in jindspre]):
+                        continue # For condition m2 on p. 100.
+
                     if increasing == None: # non-generative system
-                        iindspre = [[i]*len(inds) for i, inds in \
-                                    enumerate(ind_stack)]
                         iinds = reduce(lambda x, y: list(x) + list(y),
                                        iindspre)
+                        jinds = reduce(lambda x, y: list(x) + list(y),
+                                       jindspre)
                         yield [iinds, jinds]
-                    if increasing != None: # generative system
-                        yield generative_inds(ind_stack, increasing) 
+
+                    if increasing != None: # Generative system
+
+                        if not any([len(inds) > 1 for inds in iindspre]):
+                            continue # Won't create generating/ed indices.
+                        
+                        # Split mask into generating and generated indices.
+                        generating_iindspre = []
+                        generated_iindspre = []
+                        generating_jindspre = []
+                        generated_jindspre = []
+                        for rowinds, colinds in zip(iindspre, jindspre):
+                            if len(rowinds) > 1: 
+                                # This row has more than one entry, so should
+                                # be split.
+                                if increasing: # Increasing system over backdrop.
+                                    generating_iindspre.append(rowinds[:-1])
+                                    generating_jindspre.append(colinds[:-1])
+                                    generated_iindspre.append([rowinds[-1]])
+                                    generated_jindspre.append([colinds[-1]])
+                                else: # Decreasing system over backdrop.
+                                    generating_iindspre.append(rowinds[1:])
+                                    generating_jindspre.append(colinds[1:])
+                                    generated_iindspre.append([rowinds[0]])
+                                    generated_jindspre.append([colinds[0]])
+                            else:
+                                # This row has only one entry, so has no
+                                # generating states.
+                                generated_iindspre.append(rowinds)
+                                generated_jindspre.append(colinds)
+                        
+                        try:
+                            generating_iinds = reduce(lambda x, y: list(x) + list(y),
+                                           generating_iindspre)
+                        except:
+                            pdb.set_trace()
+                        generating_jinds = reduce(lambda x, y: list(x) + list(y),
+                                       generating_jindspre)
+                        generated_iinds = reduce(lambda x, y: list(x) + list(y),
+                                       generated_iindspre)
+                        generated_jinds = reduce(lambda x, y: list(x) + list(y),
+                                       generated_jindspre)
+
+                        yield [[generated_iinds, generated_jinds], \
+                                [generating_iinds, generating_jinds]]
+
+
                 else:
                     rind += 1
                     self.gen_inds(gen_stack, ind_stack, rind, increasing)
+
             except StopIteration:
                 gen_stack[rind] = self._powerset()
                 rind -= 1
@@ -142,13 +164,23 @@ class Generative_system(object):
 
     """
 
-    def __init__(self, maskinds):
+    def __init__(self, generated_maskinds, generating_maskinds):
+
+        maskinds = [generating_maskinds[0] + generated_maskinds[0],
+                    generating_maskinds[1] + generated_maskinds[1]]
 
         self.maskinds = maskinds
+        self.generating_maskinds = generating_maskinds
+        self.generated_maskinds = generated_maskinds
+        
+        generatednum = len(generated_maskinds[0])
+        generatingnum = len(generating_maskinds[0])
+        self.generating_sampinds = range(generatingnum)
+        self.generated_sampinds = range(generatingnum, generatingnum + generatednum)
 
-    def prob_bf(self, data):
+    def get_generation_probabilities(self, data):
         """
-        Probabilistic behavior function.
+        Get the probabilistic behavior functions.
 
         Args:
           data (numpy.ndarray): curated data of n rows by t columns, where n
@@ -168,28 +200,120 @@ class Generative_system(object):
         support_len = data.shape[1]
         sample_num = support_len - mwidth + 1
 
-        # the defaultdict will learn occurring samples as the mask scans
-        # across the data
+        # Record all sampling variables for the mask.
         d_samp_cnt = defaultdict(int)
-        for s in xrange(sample_num):
-            # scan the mask across the ordered support
+        gtng_sampvars = set()
+        gtd_sampvars = set()
+        for s in range(sample_num):
+
+            # Full mask counts.
             sampinds = [self.maskinds[0],
                         [j+s for j in self.maskinds[1]]]
             sample = data[sampinds]
             d_samp_cnt[tuple(sample)] += 1
 
+            ## Generating and generated variables observed.
+            generatinginds = [self.generating_maskinds[0],
+                                [j+s for j in self.generating_maskinds[1]]]
+            gtng_sampvars.add(tuple(data[generatinginds]))
+            generatedinds = [self.generated_maskinds[0],
+                                [j+s for j in self.generated_maskinds[1]]]
+            gtd_sampvars.add(tuple(data[generatedinds]))
+
         tot_count = 0
-        for c in d_samp_cnt.itervalues():
+        for c in d_samp_cnt.values():
             tot_count += c
         tot_count = float(tot_count)
-        sample_len = len(self.maskinds[0])
-        sample_cnt = len(d_samp_cnt)
-        # probabilistic behavior function array
-        dtype = [('s{}'.format(i),'u2') for i in range(sample_len)] + \
-                [('f', 'f4')]
-        a_bf = np.array(np.zeros(shape=(sample_cnt,),
-                        dtype=dtype))
-        for s, (sample, count) in enumerate(d_samp_cnt.iteritems()):
-            a_bf[s] = tuple(list(sample) + [count/tot_count])
+        
+        # Probabilistic behavior function array, split into generating,
+        # generated, and probability lists (same order).  This keeps variable
+        # states as tuples with integer values.  Placing generated and
+        # generating states in the same array as their probabilities forces
+        # state values to be floats.  This is one solution.
 
-        return pd.DataFrame(a_bf)
+        generated_varstates = []
+        generating_varstates = []
+        maskstate_probs = []
+        for sample, count in d_samp_cnt.items():
+            #generated_varstates.append(tuple(sample[self.generated_sampinds]))
+            generated_varstates.append(tuple([sample[si] for si in self.generated_sampinds]))
+            #generating_varstates.append(tuple(sample[self.generating_sampinds]))
+            generating_varstates.append(tuple([sample[si] for si in self.generating_sampinds]))
+            maskstate_probs.append(count/tot_count)
+
+        # Get generating marginal probabilities.
+        d_gtngsampvar_prob = {}
+        for gen_sampvar in gtng_sampvars:
+            p_gen_sampvar = 0.
+            for gtng, maskstate_prob in \
+                    zip(generating_varstates, maskstate_probs):
+                if gtng == gen_sampvar:
+                    p_gen_sampvar += maskstate_prob
+            d_gtngsampvar_prob[gen_sampvar] = p_gen_sampvar
+
+        # Get conditional probabilities for generated given generating states.
+        d_gtd_gtng_prob = {}
+        for gen_sampvar, gtd_sampvar, maskstate_prob in \
+                zip(generating_varstates, generated_varstates, maskstate_probs):
+            d_gtd_gtng_prob[(gtd_sampvar, gen_sampvar)] = \
+                    maskstate_prob/d_gtngsampvar_prob[gen_sampvar]
+
+        self.generated_varstates = generated_varstates
+        self.generating_varstates = generating_varstates
+        self.maskstate_probs = maskstate_probs
+        self.generated_sampvars = gtd_sampvars
+        self.generating_sampvars = gtng_sampvars
+        self.d_gtngsampvar_prob = d_gtngsampvar_prob
+        self.d_gtd_gtng_prob = d_gtd_gtng_prob
+
+    def get_uncertainty(self):
+        """Calculate generative uncertainty."""
+        uncertainty = 0
+        for gtng in self.generating_sampvars:
+            
+            inner_summands = []
+            for gtd in self.generated_sampvars:
+                gtd_gtng = (gtd, gtng)
+                if gtd_gtng in self.d_gtd_gtng_prob:
+                    inner_summands.append(self.d_gtd_gtng_prob[gtd_gtng])
+            innersum = sum([k*np.log2(k) for k in inner_summands])
+
+            uncertainty -= self.d_gtngsampvar_prob[gtng]*innersum
+
+        self.uncertainty = uncertainty
+
+    def get_complexity(self):
+        self.complexity = len(self.maskinds[0])
+
+    def compute_behavior_system(self, data):
+        self.get_generation_probabilities(data)
+        self.get_uncertainty()
+        self.get_complexity()
+
+def find_admissible_behavior_systems(generative_systems):
+    """
+    Isolate admissible behavior systems, using the methodology in pp.  115-120
+    in ASPS.
+
+    """
+
+    complex_sorted = sorted(it.groupby(gensyslist, lambda x: x['complexity']), 
+                            key=lambda x: x[0], reverse=True)
+
+    admissible = []
+    admissible_uncertainties = [np.inf]
+    for complexity_block in complex_sorted:
+
+        unc_sorted = sorted(complexity_block, key=lambda x: x.uncertainty,
+                reverse=False)
+        lowest_unc = unc_sorted[0].uncertainty
+
+        ## Establish upper bound of complexity in complexity-uncertainty front.
+        if any([lowest_unc <= unc in admissible_uncertainties]):
+            admissible = []
+            admissible_uncertainties = []
+        else:
+            admissible.append(unc_sorted[0])
+            admissible_uncertainties.append(lowest_unc)
+
+    return admissible
